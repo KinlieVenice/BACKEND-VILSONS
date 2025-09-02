be4edd4338734076fa9e9cb1e083cd2b0aaa941a
Kinlie Venice L. de Guzman <kinlievenicedeguzman@gmail.com>
2025-09-02 21:51:46 +0800

    added logoutController

---
+++ controllers/logoutController.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
+
+const handleLogout = async (req, res) => {
+  // On client, also delete the accessToken (frontend)
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //no content
+  const refreshToken = cookies.jwt;
+
+  const foundUser = await prisma.user.findFirst({where: { refreshToken }});
+  if (!foundUser) {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "None",
+      // secure: true,
+    });
+    return res.sendStatus(204);
+  }
+
+  await prisma.user.update({
+    where: { username: foundUser.username },
+    data: { refreshToken: "" },
+  });
+
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    // secure: true,
+    sameSite: "None",
+  });
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/roles", require("./routes/api/roles"));
