41e18d28e0f31df747f6cbf8b4f362c8310f070f
Kinlie Venice L. de Guzman <kinlievenicedeguzman@gmail.com>
2025-09-22 13:18:17 +0800

    added deleteOtherIncome route

--- controllers/otherIncomeController.js
+++ controllers/otherIncomeController.js
@@ -79,5 +79,55 @@ const editOtherIncome = async (req, res) => {
     }
 }
 
+const deleteOtherIncome = async (req, res) => {
+  if (!req.body.id) return res.status(404).json({ message: "Id is required" });
+
+  try {
+    const otherIncome = await prisma.otherIncome.findFirst({
+      where: { id: req.body.id },
+    });
+    if (!otherIncome)
+      return res
+        .status(404)
+        .json({ message: `Other income with id: ${req.body.id.id} not found` });
+
+    const needsApproval = req.approval;
+    let message;
+    const otherIncomeData = {
+      description: description ?? otherIncome.description,
+      amount: amount ?? otherIncome.amount,
+      branchId: branchId ?? otherIncome.branchId,
+      updatedByUser: req.username,
+    };
+
+    const result = await prisma.$transaction(async (tx) => {
+      const deletedOtherIncome = needsApproval
+        ? await tx.otherIncomeEdit.create({
+            data: {
+              ...otherIncomeData,
+              createdByUser: req.username,
+              otherIncomeId: otherIncome.id,
+              requestType: "delete",
+            },
+          })
+        : await tx.otherIncome.delete({
+            where: { id: otherIncome.id },
+          });
+
+      message = needsApproval
+        ? "Other income delete awaiting approval"
+        : "Other income deleted successfully";
+
+      return deletedOtherIncome;
+    });
+
+    return res.status(201).json({ message, data: result });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const getAllOtherIncomes = async(req, res);
+
 
 module.exports = { createOtherIncome, editOtherIncome }
